e0075b01727039566452862afee36754e1c1b7d4
LuizPaluski <luizgustavodelimapalauski@gmail.com>
2025-05-19 11:04:08 -0300

    Atualizacao 5

--- Lista5.php
+++ Lista5.php
@@ -2,7 +2,7 @@
 
 function Multiplo($x){
     if ($x % 4 ===  0){
-        return "true";
+        return "True";
     }else{
         return "False";
     }
@@ -12,14 +12,30 @@ $y = Multiplo($numero);
 echo $y;
 
 function ParImpar($x){
-        if($x % 2 === 0 ){
-            
-            return "Par";
-        }else{
-            
-            return "Impar";
+    if($x % 2 === 0 ){
+        return "Par";
+
+    }else{    
+        return "Impar";
         }    
     }
 $numero2 = readline("Digite um valor ");
 $z = ParImpar($numero2);
 echo $z;
+
+
+function Num ($x, $y){
+    $valor = 0;
+    for($i = $x; $i <= $y; $i++){
+        $valor += $i;
+    }
+    return $valor;
+
+    }
+// colocase o +1 e o -1 para nao incluir o valor original de x e y apenas a soma entre os valores internos
+$x = readline("Digite o valor de x ")+1; 
+$y = readline("Digite o valor de y ")-1;
+$nb = num($x, $y);
+echo "A soma dos valores e $nb";
+
+
--- Teste.php
+++ Teste.php
@@ -1,20 +1,18 @@
 <?php 
 function Num ($x, $y){
-    $Inicio = max($x, $y)+1;
-    $Fim = min($x, $y)-1;
-
     $valor = 0;
-    for($i = $Fim; $i <= $Inicio; $i++);{
+    for($i = $x; $i <= $y; $i++){
         $valor += $i;
     }
     return $valor;
 
     }
+$x = readline("Digite o valor de x ")+1;
+$y = readline("Digite o valor de y ")-1;
+$nb = num($x, $y);
+echo "A soma dos valores e $nb";
 
-$x = 1; 
-$y = 5;
-$soma = num($x, $y);
-echo $soma;
+
     
 
 
